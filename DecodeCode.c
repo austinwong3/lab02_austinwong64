5fab9c373967f6627b0717d81043cba33d0d516d
Austin Wong <austinwong@umail.ucsb.edu>
2019-01-21 22:18:34 -0800

    finished

--- DecodeCode.c
+++ DecodeCode.c
@@ -6,12 +6,23 @@ mipsinstruction decode(int value)
 	mipsinstruction instr;
 
 	// TODO: fill in the fields
-	instr.funct = 15;
-	instr.immediate = -10;
-	instr.rd = 0;
-	instr.rt = 5;
-	instr.rs = 3;
-	instr.opcode = 25;
+	instr.funct = value & 63;
+
+	instr.immediate = value & 65535;
+	instr.immediate = instr.immediate <<16;
+	instr.immediate = instr.immediate >>16;
+
+	instr.rd = value & 63488;
+	instr.rd = instr.rd >> 11;
+
+	instr.rt = value & 2031616;
+	instr.rt = instr.rt >> 16;
+
+	instr.rs = value & 65011712;
+	instr.rs = instr.rs >>21;
+
+	instr.opcode = value & 4227858432;
+	instr.opcode = instr.opcode >>26;
 
 	return instr;
 }
--- RandomCode.c
+++ RandomCode.c
@@ -3,67 +3,79 @@
 
 int multiplyBy8(int v)
 {
-	// TODO: multiply the number by 8
+	v=v << 3;
 	return v;
 }
 
 int setBit6to1(int v)
 {
-	// TODO: set bit 6 to 1
+	v = v|64;
 	return v;
 }
 
 int setBit3to0(int v)
 {
-	// TODO: set bit 3 to 0
+	v=v&(~8);
 	return v;
 }
 	
 int flipBit5(int v)
 {
-	// TODO: flip bit 5 (if it is 0, make it 1.  If 0, make 1)
+	v = v^32;
 	return v;
 }
 	
 int ifBit7is0(int v)
 {
-	// TODO: check to see if bit 7 is a 0 - return 1 if true, 0 if false
-	return v;
+	if ((v&128) == 0)
+		return 1;
+	return 0;
 }
 
 int ifBit3is1(int v)
 {
-	// check to see if bit 3 is a 1 - return 1 if true, 0 if false
-	return v;
+	if((v&8) == 8)
+		return 1;
+	return 0;
 }
 
 int unsignedBits0through5(int v)
 {
-	// return the unsigned value in bits 0 through 5
+	v = v & 63;
 	return v;
 }
 
 int unsignedBits6through9(int v)
 {
-	// return the unsigned value in bits 6 through 9
+	v = v & 960;
+	v = v >>6;
 	return v;
 }
 
 int signedBits0through5(int v)
 {
-	// return the signed value in bits 0 through 5
+	v = v & 63;
+	v = v << 26;
+	v = v >> 26;
 	return v;
 }
 
 int signedBits6through9(int v)
 {
-	// return the signed value in bits 6 through 9
+	v = v & 960;
+	v = v << 22;
+	v = v >> 28;
 	return v;
 }
 
 int setBits4through9(int v, int setValue)
 {
-	// set bits 4 through 9 in v to become setValue
+	setValue = setValue & 63;
+	setValue = setValue << 4;
+	//setValue = setValue | 4294966287;
+	v = v & ~1008;
+	
+	v = v | setValue;
 	return v;
 }
 
